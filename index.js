fe789342749538983273d80bf8d9edcc14bf4a68
vais2004 <vkawale2004@gmail.com>
2024-11-02 17:17:33 +0530

    add vercel.json

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+
 
 const {initializeDatabase}= require('./db/db.connect')
 const Movie = require('./models/movie.models');
